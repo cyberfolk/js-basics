003c522ee48f7ecdff19fa73c94a35057765b74f
cyberfolk <andrea1995ferrari.com>
2023-03-07 16:07:11 +0100

    Set script but don't work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,33 @@
-// chiediamo all'utente il suo nome e inseriamo in pagina la stringa "ciao 'nome utente'"
+/* Decrizione passaggi:
+ * 1. Chiedi all'utente il suo nome,
+ * 2. Poi chiedi il suo cognome,
+ * 3. Poi chiedi il suo colore preferito
+ * 4. Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
 
-// inserisco un prompt per chiedere all'utente il nome, salvandolo in una variabile
-let username = prompt("inserisci il tuo nome");
-// console.log il risultato del prompt
-console.log(username);
-// inserisco un h1 nell'html
+// 1. Inserisco un prompt per chiedere all'utente il nome, salvandolo in una variabile
+const name = prompt("Inserisci il tuo nome");
+// Visualizzo a console.log il risultato del prompt
+console.log(name);
 
-// seleziono e salvo in una varibaile un elemento della dom con getElementById
-let nameElement = document.getElementById("name");
-console.log(nameElement);
-// assegno all'innerHtml un nuovo valore
-nameElement.innerHTML = `Ciao ${username}`;
+// 2. Inserisco un prompt per chiedere all'utente il nome, salvandolo in una variabile
+const surname = prompt("Inserisci il tuo cognome");
+// Visualizzo a console.log il risultato del prompt
+console.log(surname);
+
+// 3. Inserisco un prompt per chiedere all'utente il nome, salvandolo in una variabile
+const color = prompt("Inserisci il tuo colore preferito");
+// Visualizzo a console.log il risultato del prompt
+console.log(color);
+
+// 3. Inserisco i dati per genrare la password
+const pwd = `${name}${surname}${color}23`;
+// Visualizzo a console.log la password insicurissima
+console.log(color);
+
+// seleziono e salvo in una varibaile l'elemento della dom con getElementById
+const pwd_box = document.getElementById("pwd_box");
+// Visualizzo a console.log il risultato di document.getElementById
+console.log(pwd_box);
+
+// 4. Visualizzo sulla pagina la password
+pwd_box.innerHTML = pwd;
