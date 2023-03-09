168bb7ec269cfe0e3dfc6d695daa6b0b11d4ff9d
cyberfolk <andrea1995ferrari.com>
2023-03-09 17:49:50 +0100

    Add const element HTML to generateBtn

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,21 +45,21 @@ console.log(`carriage_boxE = ${carriage_boxE}`);
 const generateBtn = document.getElementById("generate");
 generateBtn.addEventListener("click", function () {
   // Prelevo il valore da id_input
-  const id = document.getElementById("id_input").value;
+  const id = id_inputE.value;
   // Visualizzo a console.log il dato prelevato
   console.log(`id_input = ${id_input}`);
   // Metto il valore prelevato in id_user_box2
-  document.getElementById("id_output").innerHTML = id;
+  id_outputE.innerHTML = id;
 
   // Prelevo il valore da km_set_box
-  const km = document.getElementById("km_input").value;
+  const km = km_inputE.value;
   // Visualizzo a console.log il dato prelevato
   console.log(`km = ${km}`);
 
   // Prelevo il valore da age_input
-  const age = document.getElementById("age_input").value;
+  const age = age_inputE.value;
   // Visualizzo a console.log il risultato del prompt
-  console.log(`age_input = ${age_input}`);
+  console.log(`age = ${age}`);
 
   // Calcolo quanto dovrebbe costare il biglietto a prezzo intero
   let ticket_bill = km * EURO_AT_KM;
@@ -70,31 +70,31 @@ generateBtn.addEventListener("click", function () {
   if (age < AGE_MIN) {
     ticket_bill = ticket_bill * (1 - AGE_MIN_DISCOUNT);
     console.log(`age: ${age} < 18 ---> Applico Sconto del 20%`);
-    document.getElementById("offer_box").innerHTML = `Biglietto Under ${AGE_MIN}`;
+    offer_boxE.innerHTML = `Biglietto Under ${AGE_MIN}`;
   } else if (age > AGE_MAX) {
     ticket_bill = ticket_bill * (1 - AGE_MAX_DISCOUNT);
     console.log(`age: ${age} > 65 ---> Applico Sconto del 40%`);
-    document.getElementById("offer_box").innerHTML = `Biglietto Over ${AGE_MAX}`;
+    offer_boxE.innerHTML = `Biglietto Over ${AGE_MAX}`;
   } else {
-    document.getElementById("offer_box").innerHTML = `Biglietto Standard`;
+    offer_boxE.innerHTML = `Biglietto Standard`;
   }
 
   // Visualizzo a console.log il prezzo finale del biglietto
   console.log(`ticket_bill NOW = ${ticket_bill.toFixed(2)}€`);
   // Visualizzo sulla pagina ticket_bill nel suo apposito elemento della dom
-  document.getElementById("ticket_bill_box").innerHTML = ticket_bill.toFixed(2) + "€";
+  ticket_bill_boxE.innerHTML = ticket_bill.toFixed(2) + "€";
 
   // Genero random il codice_cp
   const cp = Math.floor(Math.random() * 100000);
   // Visualizzo a console.log il risultato del Calcolo
   console.log(`cp = ${cp}`);
   // Visualizzo sulla pagina il codice cp nel suo apposito elemento della dom
-  document.getElementById("cp_box").innerHTML = cp;
+  cp_boxE.innerHTML = cp;
 
   // Genero il numero della carrozza
   const carriage = Math.floor(Math.random() * 10 + 1);
   // Visualizzo a console.log il risultato del Calcolo
   console.log(`carriage = ${carriage}`);
   // Visualizzo sulla pagina il numero di carrozza nel suo apposito elemento della dom
-  document.getElementById("carriage_box").innerHTML = carriage;
+  carriage_boxE.innerHTML = carriage;
 });
