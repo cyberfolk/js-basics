291465192768eed5a46e3fdeec60ff3b61641b8c
cyberfolk <andrea1995ferrari.com>
2023-03-13 16:07:03 +0100

    Remove usesless part of task

--- dice/assets/js/main.js
+++ dice/assets/js/main.js
@@ -1,10 +1,4 @@
-/* MAIL
-* Chiedi all’utente la sua email,
-* controlla che sia nella lista di chi può accedere, 
-* stampa un messaggio appropriato sull’esito del controllo.
-* MAIL BONUS
-* Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
-*
+/* 
 * GIOCO DEI DADI
 * Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 * Stabilire il vincitore, in base a chi fa il punteggio più alto.
