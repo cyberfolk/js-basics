1dce668de294291d0a6c48c278674bfbb1ced8fa
cyberfolk <andrea1995ferrari.com>
2023-03-14 12:43:34 +0100

    Add Array of insert nubers

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -4,9 +4,16 @@ Il programma stampa la somma di tutti i numeri inseriti. */
 
 const RANGE_INSERT = 10;
 let sum = 0;
+let numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-for (let I = 0; I < RANGE_INSERT; I++) {
-  sum += Number(prompt("inseirici un numero"));
+for (let i = 0; i < RANGE_INSERT; i++) {
+  const num = Number(prompt("inseirici un numero"));
+  numbers[i] = num;
+  sum += num;
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(`Lo ${0}-esimo numero inserito è ${numbers[i]}`);
 }
 
 console.log(`La somma dei numeri inseriti è ${sum}`);
