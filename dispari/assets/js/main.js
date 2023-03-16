40d32eab2844eceeb8a223c3c2368d4864032800
cyberfolk <andrea1995ferrari.com>
2023-03-16 15:58:01 +0100

    add isUserWinner() without any controls. but works

--- dispari/assets/js/main.js
+++ dispari/assets/js/main.js
@@ -5,8 +5,32 @@
 * Dichiariamo chi ha vinto.
 */
 
-let usercChoice = prompt("Scegli pari o dispari?").toLowerCase;
+let userChoice = prompt("Scegli pari o dispari?").toLowerCase();
+let userNumber = Number(prompt("Inserisci un numero da uno a 5"));
+let aiNumber = randomNumber1_5();
+
+if (isUserWinner(userChoice, userNumber, aiNumber)) {
+    console.log(`VINCE UTENTE. Utente sceglie ${userChoice}. Numero Utente ${userNumber}. Numero PC ${aiNumber}`);
+} else {
+    console.log(`VINCE PC. Utente sceglie ${userChoice}. Numero Utente ${userNumber}. Numero PC ${aiNumber}`);
+}
 
 function randomNumber1_5() {
     return Math.floor((Math.random() * 5) + 1);
+}
+
+function isUserWinner(userChoice, userNumber, aiNumber) {
+    let restChoice = 0
+    const sumNumber = userNumber + aiNumber;
+    console.log(`la somma dei numeri è ${sumNumber}`);
+    if (userChoice == 'dispari') {
+        restChoice = 1;
+    }
+    console.log(`L'utente ha scelto ${userChoice} quindi il resto è ${restChoice}`);
+
+    if (sumNumber % 2 == restChoice) {
+        return true;
+    } else {
+        return false;
+    }
 }
