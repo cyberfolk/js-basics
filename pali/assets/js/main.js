a7a5c96bdc13f7370810dbf8b92b1a71452c734f
cyberfolk <andrea1995ferrari.com>
2023-03-16 15:27:07 +0100

    Add comments to describe the function

--- pali/assets/js/main.js
+++ pali/assets/js/main.js
@@ -11,11 +11,16 @@ if (isPalindrome(word)) {
 
 function isPalindrome(word) {
     let i = 0;
+    /* MIDDLE_WORD is to avoid double-checking the same couple words with the loop. */
+    /* Math.floor was inserted to isolate and discard the central character of odd words */
     const MIDDLE_WORD = Math.floor(word.length / 2);
 
+    /* Exit the loop only when you compare two different characters or finish parsing the word */
     while (word[i] === word[word.length - i - 1] && i < MIDDLE_WORD) {
         i++;
     }
 
+    /* If I exited the loop before finishing parsing the word, it means that I found two different characters. */
+    /* If i parsed the entire word mean tha the word is a palindrome */
     return ((i === MIDDLE_WORD) ? true : false);
 }
