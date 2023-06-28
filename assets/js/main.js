dd41c4f8e92028d4de9f74b30cdec4a38550596f
cyberfolk <andrea1995ferrari@gmail.com>
2023-06-28 14:46:50 +0200

    Add Readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-/* L'utente dovrà compilare i seguenti campi
- * - 1 Nome e Cognome
- * - 2 Il numero di km che vuole percorrere
- * - 3 L'età del passeggero.
- * Sulla base di queste informazioni il prgramma dovrà
- * Calcolare il prezzo totale del viaggio, secondo queste regole:
- * - Il prezzo del biglietto è definito in base ai km (0.21 € al km)
- * - Va applicato uno sconto del 20% per i minorenni
- * - Va applicato uno sconto del 40% per gli over 65.
- * L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
- * Creare 2 bottini
- * - Uno per calcolare e visualizzare i dati relativi al biglietto
- * - Uno per cancellare i dati nei vari campi
- */
-
 //#region Definisco le costanti fornite dal testo dell'esercizio ====
 const EURO_AT_KM = 0.21;
 const AGE_MIN = 18;
