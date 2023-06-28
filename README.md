a990e6f9545a490637eef3032e3c7cf8d42c8a6b
cyberfolk <andrea1995ferrari@gmail.com>
2023-06-28 14:47:16 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# js-biglietto-treno
+
+Il programma dovrà chiedere all'utente
+
+1. Il numero di km che vuole percorrere
+2. L'età del passeggero.
+
+Sulla base di queste informazioni il prgramma dovrà Calcolare il prezzo totale del viaggio, secondo queste regole:
+
+- Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- Va applicato uno sconto del 20% per i minorenni
+- Va applicato uno sconto del 40% per gli over 65.
+
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
-/* Il programma dovrà chiedere all'utente
- * 1 Il numero di km che vuole percorrere
- * 2 L'età del passeggero.
- * Sulla base di queste informazioni il prgramma dovrà
- * 3 Calcolare il prezzo totale del viaggio, secondo queste regole:
- * - Il prezzo del biglietto è definito in base ai km (0.21 € al km)
- * - Va applicato uno sconto del 20% per i minorenni
- * - Va applicato uno sconto del 40% per gli over 65.
- * 4 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
-
 // Definisco le costanti fornite dal testo dell'esercizio
 const EURO_AT_KM = 0.21;
 const AGE_MIN = 18;
