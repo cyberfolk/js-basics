cea1706970e8147e151ca62f7b9efbfc555d66bf
cyberfolk <andrea1995ferrari@gmail.com>
2023-06-28 15:57:17 +0200

    Add Readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/* Decrizione passaggi:
- * 1. Chiedi all'utente il suo nome,
- * 2. Poi chiedi il suo cognome,
- * 3. Poi chiedi il suo colore preferito
- * 4. Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
-
 // 1. Inserisco un prompt per chiedere all'utente il nome, salvandolo in una variabile
 const userName = prompt("Inserisci il tuo nome");
 // Visualizzo a console.log il risultato del prompt
