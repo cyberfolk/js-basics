9619f01fbb7fbf0c957416592f1edb40ffeb419d
cyberfolk <andrea1995ferrari@gmail.com>
2023-06-28 14:42:57 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# js-lista-spesa
+
+Data una lista della spesa,
+
+Stampare sulla pagina gli elementi della lista individualmente con un ciclo while.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
-/*
- * CONSEGNA:
- * Data una lista della spesa,
- * Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
- *
- * CONSIGLI:
- * Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
- * Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
- */
-
-const shopListItem = ["Biscotti", "Latte", "Pasta", "Prosciutto", "Peperoni"];
+const shopListItem = [
+  "Biscotti",
+  "Latte",
+  "Pasta",
+  "Prosciutto",
+  "Peperoni"
+];
 const shopListQuantity = ["2", "2", "2", "3", "5"];
 const elShopList = document.querySelector(".shop_list");
+
 let i = 0;
 
 while (i < shopListItem.length) {
